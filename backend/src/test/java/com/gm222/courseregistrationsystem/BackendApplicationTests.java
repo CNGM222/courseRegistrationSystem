4eba6d81c58524643c1f15689828390b1a388b9d
GM222 <2981869778@qq.com>
2026-09-09 18:03:19 +0800

    first commit: 空项目

---
+++ backend/src/test/java/com/gm222/courseregistrationsystem/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm222.courseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
